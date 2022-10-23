cc4ba4e1b495b14204c0987a18414aef4c74dbef
ch-saagar <108117360+ch-saagar@users.noreply.github.com>
2022-10-23 22:55:26 +0530

    Create Armstrong's_no.py

---
+++ Armstrong's_no.py
@@ -0,0 +1,12 @@
+n = int(input("Enter three digit number : "))
+sum = 0
+temp = n
+while temp > 0:
+    digit = temp % 10
+    sum += digit * digit * digit
+    temp = temp//10
+ 
+if sum==n:
+    print('It is an Armstrong number')
+else:
+    print('It is not an Armstrong number')
